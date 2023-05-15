7259db0b53c8fa71d3bc3cc71452fb847a1c8ddb
pserotini <p.serotini@gmail.com>
2023-05-15 20:33:32 -0300

    Pycharm Push Test

--- calculator.py
+++ calculator.py
@@ -2,9 +2,11 @@
 Calculator library containing basic math operations.
 """
 
+
 # Push Test
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
